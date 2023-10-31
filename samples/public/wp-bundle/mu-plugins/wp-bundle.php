787dc31e9cb83463d812c4393604711ace9aa1a8
Mark Oosting <mark@wildsea.nl>
2023-10-31 11:11:35 +0100

    Updated wp-bundle mu-plugin

--- samples/public/wp-bundle/mu-plugins/wp-bundle.php
+++ samples/public/wp-bundle/mu-plugins/wp-bundle.php
@@ -1,48 +1,20 @@
 <?php
 /**
- * Plugin Name: Symfony Wordpress Bundle Loader
+ * Plugin Name: Wildpress loader
  * Description: Load Wordpress in Symfony
  * Version: 2.2.0
- * Author: Metabolism
+ * Author: Wildsea
  */
 
-use Metabolism\WordpressBundle\WordpressBundle;
+ use Metabolism\WordpressBundle\WordpressBundle;
 
-WordpressBundle::loadPlugins();
-WordpressBundle::loadActions();
-
-// WordpressBundle::loadPlugins();
-
-// if( WordpressBundle::isLoginUrl() ){
-
-//     //load login only action
-//     if( class_exists('App\Action\LoginAction') )
-//         new App\Action\LoginAction();
-//     else
-//         new Metabolism\WordpressBundle\Action\LoginAction();
-
-//     return;
-// }
-
-// if( is_admin() )
-// {
-// 	//load back only action
-// 	if( class_exists('App\Action\AdminAction') )
-// 		new App\Action\AdminAction();
-// 	else
-// 		new Metabolism\WordpressBundle\Action\AdminAction();
-// }
-// else
-// {
-// 	//load front only action
-// 	if( class_exists('App\Action\FrontAction') )
-// 		new App\Action\FrontAction();
-// 	else
-// 		new Metabolism\WordpressBundle\Action\FrontAction();
-// }
-
-// //load both case action
-// if( class_exists('App\Action\WordpressAction') )
-// 	new App\Action\WordpressAction();
-// else
-// 	new Metabolism\WordpressBundle\Action\WordpressAction();
+ /**
+  * NOTE: THIS FILE WILL BE OVERWRITTEN BY WORDPRESS-BUNDLE
+  */
+ if (is_admin()) {
+     $kernel = new \App\Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+     $kernel->boot(); // Set up minimal DI container in admin interface
+ }
+ 
+ WordpressBundle::boostrap();
+ 
