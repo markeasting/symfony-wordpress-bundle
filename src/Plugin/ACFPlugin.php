45cb6331b4755888c29615a7509b78ac984fcc14
Mark <markeasting@gmail.com>
2023-11-17 17:25:06 +0100

    Update ACFPlugin to include wp-steroids features

--- src/Plugin/ACFPlugin.php
+++ src/Plugin/ACFPlugin.php
@@ -4,23 +4,44 @@ namespace Metabolism\WordpressBundle\Plugin;
 
 use Metabolism\WordpressBundle\Entity\Block;
 
-
 /**
  * Class Metabolism\WordpressBundle Framework
  */
-class ACFPlugin {
-
+class ACFPlugin
+{
 
     /**
-     * Add entity return format
+     * ACFPlugin constructor.
+     */
+    public function __construct()
+    {
+        $acf_settings = [
+            'autoload' => true,
+            'use_entity' => true,   // Optimized Post entity return format
+        ];
+
+        foreach ($acf_settings as $name => $value) {
+            if (acf_get_setting($name) !== $value)
+                acf_update_setting($name, $value);
+        }
+
+        add_filter('acf/validate_field', [$this, 'validateField']);
+        add_filter('block_render_callback', [$this, 'renderBlock']);
+
+        add_filter('acf/pre_load_value', [$this, 'preLoadValue'], 10, 3);
+    }
+
+    /**
+     * Add optimized 'entity' return format + use as default
+     * 
      * @param $field
      * @return array
      */
-    public function validateField($field){
+    public function validateField($field)
+    {
 
-        if( $field['name'] === 'return_format'){
-
-            if( isset($field['choices']['object'] ) )
+        if ($field['name'] === 'return_format') {
+            if (isset($field['choices']['object']))
                 $field['choices']['link'] = __('Link');
 
             $field['choices']['entity'] = __('Entity');
@@ -32,19 +53,21 @@ class ACFPlugin {
 
     /**
      * Render block
+     * 
      * @param $acf_block
      * @return void
      */
-    public static function renderBlock($acf_block){
+    public static function renderBlock($acf_block)
+    {
 
         $block = [
-            'blockName'=>$acf_block['name'],
-            'attrs' => $acf_block
+            'blockName' => $acf_block['name'],
+            'attrs' => $acf_block,
         ];
 
-        if( $image = $acf_block['data']['_preview_image']??false ){
+        if ($image = $acf_block['data']['_preview_image'] ?? false) {
 
-            echo '<img src="'.get_home_url().'/'.$image.'" style="width:100%;height:auto" class="preview_image"/>';
+            echo '<img src="' . get_home_url() . '/' . $image . '" style="width:100%;height:auto" class="preview_image"/>';
             return;
         }
 
@@ -53,13 +76,20 @@ class ACFPlugin {
         echo $block->render();
     }
 
-
     /**
-     * ACFPlugin constructor.
+     * Disable database query for non editable field
+     * 
+     * @param $unused
+     * @param $post_id
+     * @param $field
+     * @return string|null
      */
-    public function __construct()
+    public function preLoadValue($unused, $post_id, $field)
     {
-        add_filter('acf/validate_field', [$this, 'validateField']);
-		add_filter('block_render_callback', [$this, 'renderBlock']);
+
+        if ($field['type'] == 'message' || $field['type'] == 'tab')
+            return '';
+
+        return null;
     }
 }
