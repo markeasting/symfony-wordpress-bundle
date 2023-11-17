97fe0f84f39bbde22b455c6f52e73b4d80db5ed3
Mark <markeasting@gmail.com>
2023-11-17 18:30:40 +0100

    Updated PerformancePlugin

--- src/Plugin/PerformancePlugin.php
+++ src/Plugin/PerformancePlugin.php
@@ -4,41 +4,48 @@ namespace Metabolism\WordpressBundle\Plugin;
 
 use Metabolism\WordpressBundle\WordpressBundle;
 
-/**
- * Class
- */
-class PerformancePlugin {
+class PerformancePlugin
+{
 
-    public function disablePlugins(){
+    public function __construct()
+    {
+        $this->generalEnhancements();
+        $this->disablePlugins();
+    }
 
-        if( !is_admin() && !WordpressBundle::isLoginUrl() ){
+    public function generalEnhancements()
+    {
+        if (!is_admin()) {
+            remove_action('init', 'check_theme_switched', 99);
+        }
+    }
 
-            if( is_multisite() )
-                add_filter('site_option_active_sitewide_plugins',  [$this, 'disableWP2FA']);
+    public function disablePlugins()
+    {
+        if (!is_admin() && !WordpressBundle::isLoginUrl()) {
+            if (is_multisite())
+                add_filter('site_option_active_sitewide_plugins', [$this, 'disableWP2FA']);
 
             add_filter('option_active_plugins', [$this, 'disableWP2FA']);
         }
     }
 
-    public function disableWP2FA($plugins){
-
-        //disable wp-2fa because of Symfony class collision
+    /**
+     * Disable wp-2fa because of a Symfony class collision
+     *
+     * @param [type] $plugins
+     * @return void
+     */
+    public function disableWP2FA($plugins)
+    {
         $wp_2fa = "wp-2fa/wp-2fa.php";
 
-        if( $k = array_search( $wp_2fa, $plugins ) )
-            unset( $plugins[$k] );
+        if ($k = array_search($wp_2fa, $plugins))
+            unset($plugins[$k]);
 
-        if( isset($plugins[$wp_2fa]) )
-            unset( $plugins[$wp_2fa] );
+        if (isset($plugins[$wp_2fa]))
+            unset($plugins[$wp_2fa]);
 
         return $plugins;
-    }
-
-    /**
-     * constructor.
-     */
-    public function __construct(){
-
-        $this->disablePlugins();
     }
 }
