68e67261c824569af53f40a2c58ec9ef497ffa84
Mark <markeasting@gmail.com>
2023-11-17 21:39:59 +0100

    Updated SecurityPlugin

--- src/Plugin/SecurityPlugin.php
+++ src/Plugin/SecurityPlugin.php
@@ -7,23 +7,27 @@ class SecurityPlugin
 
     public function __construct() 
     {
+        // Hide login error - don't expose existing accounts
+        add_filter('login_errors', function(){
+            return __('Invalid credentials', 'wp-steroids');
+        });
+
         // Secure REST API
         add_filter('rest_endpoints', [$this, 'disable_rest_endpoints']);
         add_filter('rest_jsonp_enabled', '__return_false');
 
-        // Remove WordPress version
-        remove_action('wp_head', 'wp_generator');
-
         // Disable XMLRPC
         add_filter('xmlrpc_enabled', '__return_false');
         add_filter('xmlrpc_methods', '__return_false');
+        add_filter('pings_open', '__return_false');
+        add_filter('x_redirect_by', '__return_false' );
+
+        // Remove WordPress version
+        remove_action('wp_head', 'wp_generator');
 
         add_action('wp_footer', function() {
             wp_deregister_script('wp-embed');
         });
-
-        add_filter( 'pings_open', '__return_false');
-        add_filter( 'x_redirect_by', '__return_false' );
     }
 
     /** 
